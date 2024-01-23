886f8692613743177d215f567c6a92501732c747
Joe Rosenberg <joerosenberg2@gmail.com>
2024-01-23 21:56:42 +0000

    add config extension

--- AkkaTracing.Hosting/AkkaTracingConfigurationExtensions.cs
+++ AkkaTracing.Hosting/AkkaTracingConfigurationExtensions.cs
@@ -1,17 +1,35 @@
-﻿using Akka.Configuration;
+﻿using Akka.Actor;
+using Akka.Configuration;
 using Akka.Hosting;
 
 namespace AkkaTracing.Hosting;
 
 public static class AkkaTracingConfigurationExtensions
 {
-    public static AkkaConfigurationBuilder WithTracing(this AkkaConfigurationBuilder builder)
+    public static readonly Config AkkaTracingConfig = ConfigurationFactory.ParseString(@"
+akka {
+    actor {
+        default-mailbox {
+            mailbox-type = ""AkkaTracing.Mailbox.TracingMailbox, AkkaTracing""
+        }
+
+        default-dispatcher {
+            type = ""AkkaTracing.Dispatcher.TracingDispatcherConfigurator, AkkaTracing""
+        }
+
+        telemetry {
+            enabled = true
+        }
+    }
+    
+    scheduler {
+        implementation = ""AkkaTracing.Scheduler.TracingScheduler, AkkaTracing""
+    }
+}");
+    
+    public static AkkaConfigurationBuilder AddTracing(this AkkaConfigurationBuilder builder)
     {
-        builder.WithActors((system, registry, di) =>
-        {
-
-        });
-
+        builder.AddSetup(BootstrapSetup.Create().WithConfig(AkkaTracingConfig));
         return builder;
     }
 }
